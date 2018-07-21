ea7ee21c1d0299e3f0f02a06cbe86aefa2539f50
sunrise <Kribyxiaojie08>
2018-07-21 23:36:10 +0800

    第二章
    新建了config文件

---
+++ config.py
@@ -0,0 +1,9 @@
+"""
+-*- coding: utf-8 -*-
+@Time    : 2018/7/21 23:01
+@Author  : Sunrise.Lu
+@Email   : jie.yxy@gmail.com
+@File    : config.py
+"""
+
+DEBUG = True
--- fisher.py
+++ fisher.py
@@ -2,10 +2,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route('/hello')
 def hello():
     return 'hello,Sunrise'
 
-app.run()
+app.run(host='0.0.0.0',port=80,debug=app.config['DEBUG'])
 
