359ea10007011407d780dab963da4d9e1c87cac6
sunrise.Lu <jie.yxy@gmail.com>
2018-07-29 22:13:24 +0800

    修改验证方式
    排除关键字为空时的情况

--- app/forms/book.py
+++ app/forms/book.py
@@ -6,9 +6,9 @@
 @File    : book.py
 """
 from wtforms import Form, StringField, IntegerField
-from wtforms.validators import Length, NumberRange
+from wtforms.validators import Length, NumberRange, DataRequired
 
 
 class SearchBook(Form):
-    q = StringField(validators=[Length(min=1, max=30)])
+    q = StringField(validators=[DataRequired(), Length(min=1, max=30)])
     page = IntegerField(validators=[NumberRange(min=1, max=99)], default=1)
