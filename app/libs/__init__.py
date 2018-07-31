5d80666faf92f0a3613d106a3107a75c8795857a
sunrise <Kribyxiaojie08>
2018-07-31 14:29:25 +0800

    整理了文件结构 使用codefirst构建model层

--- app/libs/__init__.py
+++ app/libs/__init__.py
@@ -1,9 +1,8 @@
 """
 -*- coding: utf-8 -*-
-@Time    : 2018/7/21 23:01
+@Time    : 2018/7/30 22:52
 @Author  : Sunrise.Lu
 @Email   : jie.yxy@gmail.com
-@File    : config.py
+@File    : __init__.py.py
 """
 
-DEBUG = True
---
+++ app/setting.py
@@ -0,0 +1,9 @@
+"""
+-*- coding: utf-8 -*-
+@Time    : 2018/7/30 22:27
+@Author  : Sunrise.Lu
+@Email   : jie.yxy@gmail.com
+@File    : setting.py
+"""
+
+PER_PAGE = 15
