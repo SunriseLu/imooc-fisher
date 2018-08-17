c39bcfe339eded8d572b10073a6b45a95daaa893
sunrise <Kribyxiaojie08>
2018-08-17 23:53:03 +0800

    增加登录页面

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -6,12 +6,25 @@
 @File    : auth.py
 """
 from wtforms import Form, StringField, PasswordField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, ValidationError
+
+from app.models import User
 
 
-class RegisterForm(Form):
+class LoginForm(Form):
     email = StringField(validators=[DataRequired(), Length(8, 64), Email(message='电子邮件不符合规范')])
 
     password = PasswordField(validators=[DataRequired(message='密码不可以为空，请输入你的密码'), Length(6, 32)])
 
+
+class RegisterForm(LoginForm):
+
     nickname = StringField(validators=[DataRequired(), Length(2, 10, message='呢称至少需要两个字符，最多十个字符')])
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError(message='电子邮件已被注册')
+
+    def validate_nickname(self, field):
+        if User.query.filter_by(nickname=field.data).first():
+            raise ValidationError(message='用户名已经被注册')
--- app/web/auth.py
+++ app/web/auth.py
@@ -1,9 +1,10 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 
-from app.forms.auth import RegisterForm
+from app.forms.auth import RegisterForm, LoginForm
 from app.models import User
 from . import web
 from app.models import db
+
 __author__ = '七月'
 
 
@@ -15,14 +16,18 @@ def register():
         user.set_attrs(form.data)
         db.session.add(user)
         db.session.commit()
-        print('ok')
+        return redirect(url_for('web.login'))
+
     return render_template('auth/register.html',
                            form=form)
 
 
-@web.route('/login')
+@web.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        pass
+    return render_template('auth/login.html', form=form)
 
 
 @web.route('/reset/password', methods=['GET', 'POST'])
